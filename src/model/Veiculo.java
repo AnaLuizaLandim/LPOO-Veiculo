b0bde6be90f8337b8310b8348cb8deab2812a00a
Ana Luiza <Ana Luiza@DESKTOP-Q8RGN57>
2025-09-04 00:38:25 -0300

    Tela Cadastro Implementada conforme a aula.

--- src/model/Veiculo.java
+++ src/model/Veiculo.java
@@ -7,7 +7,86 @@ package model;
 /**
  *
  * @author Ana Luiza
- */
+ */ 
 public class Veiculo {
+    String placa;
+    String cor;
+    int anoFabricacao;
+    int anoModelo;
+    double valor;
+    Marca marca;
+    Modelo modelo;
+
+    public Veiculo(String placa, String cor, int anoFabricacao, int anoModelo, double valor, Marca marca, Modelo modelo) {
+        this.placa = placa;
+        this.cor = cor;
+        this.anoFabricacao = anoFabricacao;
+        this.anoModelo = anoModelo;
+        this.valor = valor;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public int getAnoModelo() {
+        return anoModelo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public Modelo getModelo() {
+        return modelo;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public void setAnoModelo(int anoModelo) {
+        this.anoModelo = anoModelo;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(Modelo modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Veiculo{" + "placa=" + placa + ", cor=" + cor + ", anoFabricacao=" + anoFabricacao + ", anoModelo=" + anoModelo + ", valor=" + valor + ", marca=" + marca + ", modelo=" + modelo + '}';
+    }
     
 }
+
